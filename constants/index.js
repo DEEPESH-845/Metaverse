c3bf8b5704d16b2dc4a41f628a66939a370a601d
DEEPESH-845 <dkakkar_be23@thapar.edu>
2024-08-11 13:24:16 +0530

    Commit

---
+++ constants/index.js
@@ -0,0 +1,92 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'The Hogwarts',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'The Upside Down',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'Kadirojo Permai',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'Paradise Island',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'Hawkins Labs',
+  },
+];
+
+export const startingFeatures = [
+  'Find a world that suits you and you want to enter',
+  'Enter the world by taking a Deep Breath',
+  'No need to beat around the bush, just stay on the gas and have fun',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'A new world',
+    subtitle:
+        'we have the latest update with new world for you to try never mind',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'More realistic',
+    subtitle:
+        'In the latest update, your eyes are narrow, making the world more realistic than ever',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'Meta Unveils "Horizon Worlds" Expansion:',
+    subtitle:
+        'Meta latest expansion of "Horizon Worlds" introduces new interactive environments and customizable virtual spaces, allowing users to create and share their own immersive worlds with friends.',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: 'Decentraland Hosts Virtual Fashion Week',
+    subtitle:
+        'Decentraland is turning heads with its first Virtual Fashion Week, featuring digital runway shows and exclusive virtual fashion collections from top designers like Gucci and Dolce & Gabbana.',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'With one platform you can explore the whole world virtually',
+    subtitle:
+        'By using advanced technology, such as virtual reality (VR) or augmented reality (AR), the platform offers a comprehensive and immersive way to visit different places, cultures, and environments all within one digital space.',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+    link:'https://x.com/Deepesh_845/'
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+    link: 'https://www.linkedin.com/in/deepesh-kakkar/'
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+    link:'https://www.instagram.com/deepesh_0123/'
+  },
+  {
+    name: 'Discord',
+    url: '/icons8-discord.svg',
+    link: 'https://discord.com/channels/@me/'
+  },
+];
